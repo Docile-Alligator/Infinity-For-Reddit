3d50afd1396fcf80d11ba1fb43ce1194a7f65bf0
Alex Ning <chineseperson5@gmail.com>
2019-08-26 23:37:42 +0800

    Minor bugs fixed.

--- app/src/main/java/ml/docilealligator/infinityforreddit/Utils.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/Utils.java
@@ -2,7 +2,7 @@ package ml.docilealligator.infinityforreddit;
 
 class Utils {
     static String addSubredditAndUserLink(String markdown) {
-        return markdown.replaceAll("(?<!\\w)/+u/\\w+/*", "[$0]($0)")
-                .replaceAll("(?<!\\w)/+r/\\w+/*", "[$0]($0)");
+        return markdown.replaceAll("(?<!\\w)/*u/\\w+/*", "[$0]($0)")
+                .replaceAll("(?<!\\w)/*r/\\w+/*", "[$0]($0)");
     }
 }
