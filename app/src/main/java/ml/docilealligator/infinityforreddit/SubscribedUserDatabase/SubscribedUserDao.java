d30afcb961a8cd81ed00ad51de459b05fe708dbd
Alex Ning <chineseperson5@gmail.com>
2020-08-26 15:58:38 +0800

    Add COLLATE NOCASE to some sqlite syntaxes.

--- app/src/main/java/ml/docilealligator/infinityforreddit/SubscribedUserDatabase/SubscribedUserDao.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/SubscribedUserDatabase/SubscribedUserDao.java
@@ -13,18 +13,18 @@ public interface SubscribedUserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(SubscribedUserData subscribedUserData);
 
-    @Query("SELECT * FROM subscribed_users WHERE username = :accountName ORDER BY name COLLATE NOCASE ASC")
+    @Query("SELECT * FROM subscribed_users WHERE username = :accountName COLLATE NOCASE ORDER BY name COLLATE NOCASE ASC")
     LiveData<List<SubscribedUserData>> getAllSubscribedUsers(String accountName);
 
-    @Query("SELECT * FROM subscribed_users WHERE username = :accountName ORDER BY name COLLATE NOCASE ASC")
+    @Query("SELECT * FROM subscribed_users WHERE username = :accountName COLLATE NOCASE ORDER BY name COLLATE NOCASE ASC")
     List<SubscribedUserData> getAllSubscribedUsersList(String accountName);
 
-    @Query("SELECT * FROM subscribed_users WHERE username = :accountName AND is_favorite = 1 ORDER BY name COLLATE NOCASE ASC")
+    @Query("SELECT * FROM subscribed_users WHERE username = :accountName COLLATE NOCASE AND is_favorite = 1 ORDER BY name COLLATE NOCASE ASC")
     LiveData<List<SubscribedUserData>> getAllFavoriteSubscribedUsers(String accountName);
 
-    @Query("SELECT * FROM subscribed_users WHERE name = :name AND username = :accountName COLLATE NOCASE LIMIT 1")
+    @Query("SELECT * FROM subscribed_users WHERE name = :name COLLATE NOCASE AND username = :accountName COLLATE NOCASE LIMIT 1")
     SubscribedUserData getSubscribedUser(String name, String accountName);
 
-    @Query("DELETE FROM subscribed_users WHERE name = :name AND username = :accountName")
+    @Query("DELETE FROM subscribed_users WHERE name = :name COLLATE NOCASE AND username = :accountName COLLATE NOCASE")
     void deleteSubscribedUser(String name, String accountName);
 }
