d86adb72360fccdff50a7c3b0608baa9247a6b48
Alex Ning <chineseperson5@gmail.com>
2020-10-28 17:28:25 +0800

    Update translation contributions.

--- app/src/main/java/ml/docilealligator/infinityforreddit/Settings/Translation.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/Settings/Translation.java
@@ -17,23 +17,25 @@ public class Translation {
 
     public static ArrayList<Translation> getTranslationContributors() {
         ArrayList<Translation> translationContributors = new ArrayList<>();
-        translationContributors.add(new Translation("简体中文", "Gloria, Justin, Steps", R.drawable.flag_china));
+        translationContributors.add(new Translation("български", "Кристиян", R.drawable.flag_bulgaria));
+        translationContributors.add(new Translation("简体中文", "cdggqa, Gloria, Justin, Steps", R.drawable.flag_china));
         translationContributors.add(new Translation("繁體中文", "", R.drawable.flag_china));
         translationContributors.add(new Translation("Hrvatski", "Josip", R.drawable.flag_croatia));
-        translationContributors.add(new Translation("Française", "367, oursonbleu, Owen, pinembour", R.drawable.flag_france));
-        translationContributors.add(new Translation("Deutsche", "Jorge, Netto Hikari, translatewingman, translatorwiz", R.drawable.flag_germany));
+        translationContributors.add(new Translation("Nederlands", "Khawkfist", R.drawable.flag_netherlands));
+        translationContributors.add(new Translation("Française", "367, Finn Olmsted, oursonbleu, Owen, pinembour", R.drawable.flag_france));
+        translationContributors.add(new Translation("Deutsche", "Jorge, Netto Hikari, Nikodiamond3, translatewingman, translatorwiz", R.drawable.flag_germany));
         translationContributors.add(new Translation("Ελληνικά", "Marios, Viktor", R.drawable.flag_greece));
         translationContributors.add(new Translation("हिंदी", "a, Arya, charu, Mrigendra Bhandari, raghav, Sachin, Ved", R.drawable.flag_india));
         translationContributors.add(new Translation("Magyar", "Zoltan", R.drawable.flag_hungary));
-        translationContributors.add(new Translation("Italiana", "Daniele Basso, DanOlivaw, Gianni00palmieri, Gillauino, ztiaa", R.drawable.flag_italy));
-        translationContributors.add(new Translation("日本語", "Issa, Mrigendra Bhandari, Ryan", R.drawable.flag_japan));
+        translationContributors.add(new Translation("Italiana", "Daniele Basso, DanOlivaw, Gianni00palmieri, Gillauino, Matisse, Simoneg. work, ztiaa", R.drawable.flag_italy));
+        translationContributors.add(new Translation("日本語", "Hira, Issa, Mrigendra Bhandari, Ryan", R.drawable.flag_japan));
         translationContributors.add(new Translation("한국어", "noname", R.drawable.flag_south_korea));
         translationContributors.add(new Translation("norsk", "", R.drawable.flag_norway));
-        translationContributors.add(new Translation("Português", "Bruno Guerreiro, Lucas", R.drawable.flag_portugal));
+        translationContributors.add(new Translation("Português", "Bruno Guerreiro, Lucas, Ricky", R.drawable.flag_portugal));
         translationContributors.add(new Translation("Português (BR)", "Laura Vasconcellos Pereira Felippe, Ricky, Super_Iguanna", R.drawable.flag_brazil));
         translationContributors.add(new Translation("Română", "", R.drawable.flag_romania));
         translationContributors.add(new Translation("русский язык", "aveblazer, flexagoon, Georgiy, Vova", R.drawable.flag_russia));
-        translationContributors.add(new Translation("Español", "Jorge, Miguel, mvstermoe, Nana Snixx, Sofia Flores", R.drawable.flag_spain));
+        translationContributors.add(new Translation("Español", "Alejandro, Jorge, Miguel, mvstermoe, Nana Snixx, Sofia Flores", R.drawable.flag_spain));
         translationContributors.add(new Translation("svenska", "Marcus Nordberg", R.drawable.flag_sweden));
         translationContributors.add(new Translation("Türk", "Emir481, Faoiltiarna, Mehmet Yavuz, Serif", R.drawable.flag_turkey));
         translationContributors.add(new Translation("Tiếng Việt", "Kai, Laezzy, Lmao, Ryan, viecdet69", R.drawable.flag_vietnam));
