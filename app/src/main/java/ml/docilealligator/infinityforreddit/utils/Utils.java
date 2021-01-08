0887b534269e17678fda91fee0bf65ad7249170d
Alex Ning <chineseperson5@gmail.com>
2021-01-08 13:59:27 +0800

    Fix regex for supporting snudown's header.

--- app/src/main/java/ml/docilealligator/infinityforreddit/utils/Utils.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/utils/Utils.java
@@ -40,7 +40,8 @@ public class Utils {
         StringBuilder regexed = new StringBuilder(markdown
                 .replaceAll("((?<=[\\s])|^)/[rRuU]/[\\w-]+/{0,1}", "[$0](https://www.reddit.com$0)")
                 .replaceAll("((?<=[\\s])|^)[rRuU]/[\\w-]+/{0,1}", "[$0](https://www.reddit.com/$0)")
-                .replaceAll("\\^{2,}", "^").replaceAll("#(?![# ])", "# "));
+                .replaceAll("\\^{2,}", "^")
+                .replaceAll("(?<![\\[\\(])#(?![# ])(?![\\w\\s]*[\\]\\)])", "# "));
 
         //Fix superscript
         int startIndex = regexed.indexOf("^");
