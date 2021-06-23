866db8c68120d3a39923874e779cd976854a277e
Alex Ning <chineseperson5@gmail.com>
2021-06-23 13:50:17 +0800

    Fix Material You themeing.

--- app/src/main/java/ml/docilealligator/infinityforreddit/utils/MaterialYouUtils.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/utils/MaterialYouUtils.java
@@ -46,6 +46,7 @@ public class MaterialYouUtils {
                             .putInt(CustomThemeSharedPreferencesUtils.BACKGROUND_COLOR, backgroundColor)
                             .putInt(CustomThemeSharedPreferencesUtils.CARD_VIEW_BACKGROUND_COLOR, cardViewBackgroundColor)
                             .putInt(CustomThemeSharedPreferencesUtils.COMMENT_BACKGROUND_COLOR, cardViewBackgroundColor)
+                            .putInt(CustomThemeSharedPreferencesUtils.AWARDED_COMMENT_BACKGROUND_COLOR, cardViewBackgroundColor)
                             .putInt(CustomThemeSharedPreferencesUtils.BOTTOM_APP_BAR_BACKGROUND_COLOR, colorPrimaryInt)
                             .putInt(CustomThemeSharedPreferencesUtils.NAV_BAR_COLOR, colorPrimaryInt)
                             .putInt(CustomThemeSharedPreferencesUtils.PRIMARY_TEXT_COLOR, backgroundColorAppropriateTextColor)
