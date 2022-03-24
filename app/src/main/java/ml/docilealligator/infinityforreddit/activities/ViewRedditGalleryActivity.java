c87b724a242f327186435fe36f67055985e48a0d
Docile-Alligator <chineseperson5@gmail.com>
2022-03-24 14:00:44 +0800

    Fix captions and urls hidden by the navigation bar in ViewRedditGalleryImageorGifFragment.

--- app/src/main/java/ml/docilealligator/infinityforreddit/activities/ViewRedditGalleryActivity.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/activities/ViewRedditGalleryActivity.java
@@ -16,7 +16,6 @@ import android.os.Handler;
 import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -137,11 +136,6 @@ public class ViewRedditGalleryActivity extends AppCompatActivity implements SetA
         setContentView(R.layout.activity_view_reddit_gallery);
 
         ButterKnife.bind(this);
-
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         useBottomAppBar = sharedPreferences.getBoolean(SharedPreferencesUtils.USE_BOTTOM_TOOLBAR_IN_MEDIA_VIEWER, false);
 
--- app/src/main/java/ml/docilealligator/infinityforreddit/fragments/ViewRedditGalleryImageOrGifFragment.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/fragments/ViewRedditGalleryImageOrGifFragment.java
@@ -215,6 +215,10 @@ public class ViewRedditGalleryImageOrGifFragment extends Fragment {
         });
 
         loadImage();
+        activity.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         imageView.setOnClickListener(view -> {
             if (isActionBarHidden) {
