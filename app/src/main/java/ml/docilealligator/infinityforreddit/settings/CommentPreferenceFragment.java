67b6906ead02ceb7d7e46f28ae5c27a510956038
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2022-11-14 22:01:31 +1100

    New option: Settings->Interface->Comment->Comment Divider Type.

--- app/src/main/java/ml/docilealligator/infinityforreddit/settings/CommentPreferenceFragment.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/settings/CommentPreferenceFragment.java
@@ -3,7 +3,9 @@ package ml.docilealligator.infinityforreddit.settings;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.ListPreference;
 import androidx.preference.SeekBarPreference;
+import androidx.preference.SwitchPreference;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -25,7 +27,17 @@ public class CommentPreferenceFragment extends CustomFontPreferenceFragmentCompa
 
         ((Infinity) activity.getApplication()).getAppComponent().inject(this);
 
+        SwitchPreference showCommentDividerSwitchPreference = findPreference(SharedPreferencesUtils.SHOW_COMMENT_DIVIDER);
+        ListPreference commentDividerTypeListPreference = findPreference(SharedPreferencesUtils.COMMENT_DIVIDER_TYPE);
         SeekBarPreference showFewerToolbarOptionsThresholdSeekBarPreference = findPreference(SharedPreferencesUtils.SHOW_FEWER_TOOLBAR_OPTIONS_THRESHOLD);
+
+        if (showCommentDividerSwitchPreference != null && commentDividerTypeListPreference != null) {
+            commentDividerTypeListPreference.setVisible(sharedPreferences.getBoolean(SharedPreferencesUtils.SHOW_COMMENT_DIVIDER, false));
+            showCommentDividerSwitchPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                commentDividerTypeListPreference.setVisible((Boolean) newValue);
+                return true;
+            });
+        }
 
         if (showFewerToolbarOptionsThresholdSeekBarPreference != null) {
             showFewerToolbarOptionsThresholdSeekBarPreference.setSummary(getString(R.string.settings_show_fewer_toolbar_options_threshold_summary, sharedPreferences.getInt(SharedPreferencesUtils.SHOW_FEWER_TOOLBAR_OPTIONS_THRESHOLD, 5)));
