f994e4204492eaadbbcd02f78806e89af9500265
Sergei Kozelko <kozelkos@yandex.ru>
2022-11-17 00:55:16 +0800

    Remove unused DownloadMediaEvent

--- app/src/main/java/ml/docilealligator/infinityforreddit/events/DownloadMediaEvent.java
+++
@@ -1,9 +0,0 @@
-package ml.docilealligator.infinityforreddit.events;
-
-public class DownloadMediaEvent {
-    public boolean isSuccessful;
-
-    public DownloadMediaEvent(boolean isSuccessful) {
-        this.isSuccessful = isSuccessful;
-    }
-}
