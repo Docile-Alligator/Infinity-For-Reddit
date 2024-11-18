f3905b3d4f29d02c372a61414e881e1b18b3cea2
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2024-11-18 17:18:21 -0500

    Fix padding.

--- app/src/main/java/ml/docilealligator/infinityforreddit/activities/ModmailActivity.kt
+++ app/src/main/java/ml/docilealligator/infinityforreddit/activities/ModmailActivity.kt
@@ -6,14 +6,15 @@ import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.add
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -29,16 +30,14 @@ import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
 import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
 import androidx.compose.material3.adaptive.navigation.ThreePaneScaffoldNavigator
 import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
-import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
-import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
 import androidx.compose.material3.pulltorefresh.PullToRefreshState
-import androidx.compose.material3.pulltorefresh.pullToRefreshIndicator
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
@@ -99,40 +98,37 @@ class ModmailActivity : BaseActivity() {
                     )
                 },
                 containerColor = Color(mCustomThemeWrapper.backgroundColor),
-                contentWindowInsets = WindowInsets(0.dp)
             ) { innerPadding ->
-                Toast.makeText(this, "S " + innerPadding.calculateTopPadding(), Toast.LENGTH_SHORT).show()
-                Column(
-                    modifier = Modifier.padding(top = innerPadding.calculateTopPadding()),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    val listState: LazyListState = rememberLazyListState()
-                    val navigator = rememberListDetailPaneScaffoldNavigator<Conversation>()
+                val listState: LazyListState = rememberLazyListState()
+                val navigator = rememberListDetailPaneScaffoldNavigator<Conversation>()
 
-                    val pagingItems: LazyPagingItems<Conversation> = conversationViewModel.flow.collectAsLazyPagingItems()
+                val pagingItems: LazyPagingItems<Conversation> = conversationViewModel.flow.collectAsLazyPagingItems()
 
-                    BackHandler(navigator.canNavigateBack()) {
-                        navigator.navigateBack()
-                    }
+                BackHandler(navigator.canNavigateBack()) {
+                    navigator.navigateBack()
+                }
 
-                    ListDetailPaneScaffold(
-                        modifier = Modifier.padding(16.dp),
-                        directive = navigator.scaffoldDirective,
-                        value = navigator.scaffoldValue,
-                        listPane = {
-                            AnimatedPane {
-                                ConversationListView(pagingItems, navigator, listState)
-                            }
-                        },
-                        detailPane = {
-                            AnimatedPane {
-                                navigator.currentDestination?.content?.let {
-                                    ConversationDetailsView(it)
-                                }
+                ListDetailPaneScaffold(
+                    modifier = Modifier.padding(
+                        top = innerPadding.calculateTopPadding(),
+                        start = 16.dp,
+                        end = 16.dp
+                    ),
+                    directive = navigator.scaffoldDirective,
+                    value = navigator.scaffoldValue,
+                    listPane = {
+                        AnimatedPane {
+                            ConversationListView(pagingItems, navigator, listState)
+                        }
+                    },
+                    detailPane = {
+                        AnimatedPane {
+                            navigator.currentDestination?.content?.let {
+                                ConversationDetailsView(it)
                             }
                         }
-                    )
-                }
+                    }
+                )
             }
         }
     }
@@ -174,6 +170,8 @@ class ModmailActivity : BaseActivity() {
                 LazyColumn(
                     verticalArrangement = Arrangement.spacedBy(16.dp),
                     state = listState,
+                    contentPadding = WindowInsets(top = 16.dp).add(WindowInsets.navigationBars).asPaddingValues(),
+                    modifier = Modifier.clipToBounds()
                 ) {
                     items(count = pagingItems.itemCount) { index: Int ->
                         val conversation = pagingItems[index]
