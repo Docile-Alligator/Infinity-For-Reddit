b3cbe38b574805ddd75a137101ab2c62505818d1
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2025-07-28 22:59:55 -0400

    Fix always opening the first image in a gallery.

--- app/src/main/java/ml/docilealligator/infinityforreddit/adapters/PostRecyclerViewAdapter.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/adapters/PostRecyclerViewAdapter.java
@@ -3564,7 +3564,7 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
                                     if (position >= 0) {
                                         if (post != null) {
                                             markPostRead(post, true);
-                                            openMedia(post, layoutManager.findFirstVisibleItemPosition());
+                                            openMedia(post, layoutManager.findFirstVisibleItemPosition(), false);
                                         }
                                     }
                                 }
