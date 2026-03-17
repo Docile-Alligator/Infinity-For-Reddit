ca505e927884b336b5dcc1c1c28696a853b08e05
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2026-03-16 20:42:29 -0400

    Custom error in Webview.

--- app/src/main/java/ml/docilealligator/infinityforreddit/utils/Utils.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/utils/Utils.java
@@ -367,6 +367,24 @@ public final class Utils {
         return false;
     }
 
+    public static boolean isConnectedToInternet(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Network network = connectivityManager.getActiveNetwork();
+                if (network == null) {
+                    return false;
+                }
+                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+                return networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+            } else {
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                return networkInfo != null && networkInfo.isConnected();
+            }
+        }
+        return false;
+    }
+
     public static void displaySortTypeInToolbar(SortType sortType, Toolbar toolbar) {
         if (sortType != null) {
             if (sortType.getTime() != null) {
