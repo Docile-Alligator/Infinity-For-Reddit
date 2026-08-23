26d96c44545ffb379d6e00ed378e1366f82548bb
Itamrac <114347811+Itamrac@users.noreply.github.com>
2026-08-23 22:22:19 +0200

    Fix issue 1959 by deferring DNS resolution to actual connection time

--- app/src/main/java/ml/docilealligator/infinityforreddit/ProxyEnabledGlideModule.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/ProxyEnabledGlideModule.java
@@ -36,7 +36,7 @@ public class ProxyEnabledGlideModule extends AppGlideModule {
                 String proxyHost = mProxySharedPreferences.getString(SharedPreferencesUtils.PROXY_HOSTNAME, "127.0.0.1");
                 int proxyPort = Integer.parseInt(mProxySharedPreferences.getString(SharedPreferencesUtils.PROXY_PORT, "1080"));
 
-                InetSocketAddress proxyAddr = new InetSocketAddress(proxyHost, proxyPort);
+                InetSocketAddress proxyAddr = InetSocketAddress.createUnresolved(proxyHost, proxyPort);
                 Proxy proxy = new Proxy(proxyType, proxyAddr);
                 builder.proxy(proxy);
             }
