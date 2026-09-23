42c5b9eadb05d7d1ab36d50d826002eeaea00d39
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2026-09-23 12:25:03 -0400

    Fix white background when starting ReminderListingActivity.

--- app/src/main/java/ml/docilealligator/infinityforreddit/activities/ReminderListingActivity.kt
+++ app/src/main/java/ml/docilealligator/infinityforreddit/activities/ReminderListingActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -261,6 +262,11 @@ class ReminderListingActivity : BaseActivity() {
                                 }
                             }
                         }
+                    } ?: run {
+                        Box(modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color(LocalAppTheme.current.backgroundColor))
+                        )
                     }
 
                     if (showReminderOptionSheet) {
